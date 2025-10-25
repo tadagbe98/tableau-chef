49fbab9712c5ba21279a13a643ca2b86fac7a27a
TADAGBE LANDRY MASSOLOKONON <landry.massolokonon@groupecerco.com>
2025-10-25 22:58:02 +0000

    Try fixing this error: `Runtime Error: Error: Image Optimization using t

--- next.config.ts
+++ next.config.ts
@@ -13,7 +13,7 @@ const nextConfig: NextConfig = {
     dangerouslyAllowSVG: true,
     contentDispositionType: 'attachment',
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-    unoptimized: false,
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
